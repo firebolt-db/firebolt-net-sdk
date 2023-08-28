353505117b58bc559338ccc2c33acca5b4315cc1
alexradzin <alexander.radzin@firebolt.io>
2023-08-28 10:39:48 +0300

    added comment

--- FireboltDotNetSdk.Tests/Integration/IntegrationTest.cs
+++ FireboltDotNetSdk.Tests/Integration/IntegrationTest.cs
@@ -22,12 +22,12 @@ namespace FireboltDotNetSdk.Tests
             return default_value;
         }
 
-        protected static string Database = EnvWithDefault("FIREBOLT_DATABASE");
+        protected static string Database = EnvWithDefault("FIREBOLT_DATABASE", "alex_test2_2");
         protected static string Endpoint = EnvWithDefault("FIREBOLT_ENDPOINT", "https://api.dev.firebolt.io");
         protected static string Env = EnvWithDefault("FIREBOLT_ENV", "dev");
-        protected static string Account = EnvWithDefault("FIREBOLT_ACCOUNT", "firebolt");
-        protected static string EngineName = EnvWithDefault("FIREBOLT_ENGINE_NAME");
-        protected static string ClientId = EnvWithDefault("FIREBOLT_CLIENT_ID");
-        protected static string ClientSecret = EnvWithDefault("FIREBOLT_CLIENT_SECRET");
+        protected static string Account = EnvWithDefault("FIREBOLT_ACCOUNT", "developer");
+        protected static string EngineName = EnvWithDefault("FIREBOLT_ENGINE_NAME", "alex_test2_2_Ingest");
+        protected static string ClientId = EnvWithDefault("FIREBOLT_CLIENT_ID", "fcJFhHEFTcTKlenUd2zNXFoVoR501BtF");
+        protected static string ClientSecret = EnvWithDefault("FIREBOLT_CLIENT_SECRET", "FigJDVxl1ZceNK9pJ7b-Y3Q9_kV3T8pOv7stSNuh-d0ujIdH87RAY65xFe8sNZBv");
     }
 }
