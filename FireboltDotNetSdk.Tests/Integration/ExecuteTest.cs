c66f43484215ecb898418107c0d88a76325c663a
alexradzin <alexander.radzin@firebolt.io>
2024-04-08 09:47:29 +0300

    increased sequence size for long test

--- FireboltDotNetSdk.Tests/Integration/ExecuteTest.cs
+++ FireboltDotNetSdk.Tests/Integration/ExecuteTest.cs
@@ -65,7 +65,7 @@ namespace FireboltDotNetSdk.Tests
             DbCommand command = conn.CreateCommand();
             command.CommandTimeout = 0; // make command timeout unlimited
             // Use more rows for FB2.0 since it seems to be faster
-            var max = UserName == null ? 300000000000 : 250000000000;
+            var max = UserName == null ? 400000000000 : 250000000000;
             command.CommandText = "SELECT checksum(*) FROM generate_series(1, @max)";
             command.Prepare();
             command.Parameters.Add(CreateParameter(command, "@max", max));
