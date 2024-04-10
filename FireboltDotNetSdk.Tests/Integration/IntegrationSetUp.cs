ac622b41d75112ec4b12bf909355e0bd117b48a9
alexradzin <alexander.radzin@firebolt.io>
2024-04-10 09:36:31 +0300

    ability to create DB and engine inside integration test for engine v2

---
+++ FireboltDotNetSdk.Tests/Integration/IntegrationSetUp.cs
@@ -0,0 +1,77 @@
+using System;
+using NUnit.Framework;
+using FireboltDotNetSdk.Exception;
+using FireboltDotNetSdk.Client;
+using System.Data;
+using System.Data.Common;
+using System.Text;
+using static System.Environment;
+
+namespace FireboltDotNetSdk.Tests
+{
+    [SetUpFixture]
+    internal class IntegrationSetUp //: IntegrationTest
+    {
+        private string name = "integration_testing__" + DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+        private FireboltConnection Connection = null!;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            if (!bool.Parse(GetEnvironmentVariable("FIREBOLT_RUN_SETUP") ?? "false"))
+            {
+                return;
+            }
+            string connectionString = CreateConnectionString(new Tuple<string, string?>[]
+            {
+                Tuple.Create<string, string?>("Env", GetEnvironmentVariable("FIREBOLT_ENV") ?? "staging"),
+                Tuple.Create<string, string?>("Account", GetEnvironmentVariable("FIREBOLT_ACCOUNT") ?? "infra-engines-v2"),
+                Tuple.Create<string, string?>("ClientId", GetEnvironmentVariable("FIREBOLT_CLIENT_ID")),
+                Tuple.Create<string, string?>("ClientSecret", GetEnvironmentVariable("FIREBOLT_CLIENT_SECRET"))
+            });
+            Connection = new FireboltConnection(connectionString);
+            Connection.Open();
+            Perform("FIREBOLT_ENGINE_NAME", "ENGINE", name, "CREATE");
+            Perform("FIREBOLT_DATABASE", "DATABASE", name, "CREATE");
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            if (Connection == null)
+            {
+                return;
+            }
+            Perform("FIREBOLT_ENGINE_NAME", "ENGINE", name, "STOP", "DROP");
+            Perform("FIREBOLT_DATABASE", "DATABASE", name, "DROP");
+            Connection.Close();
+        }
+
+        private void Perform(string propertyName, string entityType, string entityName, params string[] actions)
+        {
+            string? propertyValue = GetEnvironmentVariable(propertyName);
+            if (propertyValue != null && propertyValue != "")
+            {
+                return;
+            }
+            foreach (string action in actions)
+            {
+                CreateCommand($"{action} {entityType} {entityName}").ExecuteNonQuery();
+            }
+            SetEnvironmentVariable(propertyName, entityName);
+        }
+
+        private DbCommand CreateCommand(string sql)
+        {
+            DbCommand command = Connection.CreateCommand();
+            command.CommandText = sql;
+            return command;
+        }
+
+        private string CreateConnectionString(params Tuple<string, string?>[] more)
+        {
+            return string.Join(";", more.Where(p => p.Item2 != null).Select(p => p.Item1 + "=" + p.Item2));
+        }
+    }
+}
+
--- FireboltDotNetSdk.Tests/Integration/IntegrationTest.cs
+++ FireboltDotNetSdk.Tests/Integration/IntegrationTest.cs
@@ -22,16 +22,16 @@ namespace FireboltDotNetSdk.Tests
             }
             return default_value;
         }
-        protected static string Database = EnvWithDefault("FIREBOLT_DATABASE");
-        protected static string? Endpoint = GetEnvironmentVariable("FIREBOLT_ENDPOINT");
-        protected static string Env = EnvWithDefault("FIREBOLT_ENV", "dev");
+        protected static string Database = "";
+        protected static string? Endpoint;
+        protected static string? Env;
         // Endpoint is not specified by CI/CD (YAML) for v2 where account and engine name are mandatory.
-        protected static string? Account = Endpoint == null ? EnvWithDefault("FIREBOLT_ACCOUNT") : GetEnvironmentVariable("FIREBOLT_ACCOUNT");
-        protected static string? Engine = Endpoint == null ? EnvWithDefault("FIREBOLT_ENGINE_NAME") : GetEnvironmentVariable("FIREBOLT_ENGINE_NAME");
-        protected static string? ClientId = GetEnvironmentVariable("FIREBOLT_CLIENT_ID");
-        protected static string? ClientSecret = GetEnvironmentVariable("FIREBOLT_CLIENT_SECRET");
-        protected static string? UserName = GetEnvironmentVariable("FIREBOLT_USERNAME");
-        protected static string? Password = GetEnvironmentVariable("FIREBOLT_PASSWORD");
+        protected static string? Account;
+        protected static string? Engine;
+        protected static string? ClientId;
+        protected static string? ClientSecret;
+        protected static string? UserName;
+        protected static string? Password;
 
         protected static string ConnectionString(params Tuple<string, string?>[] more)
         {
@@ -46,6 +46,7 @@ namespace FireboltDotNetSdk.Tests
             }
             return string.Join(";", conf.Where(p => p.Value != null).Select(p => p.Key + "=" + p.Value));
         }
+
         protected static string ConnectionStringWithout(params string[] names)
         {
             return ConnectionString(Tuples(names));
@@ -56,17 +57,33 @@ namespace FireboltDotNetSdk.Tests
             return names.Select(name => new Tuple<string, string?>(name, null)).ToArray();
         }
 
-        protected static IDictionary<string, string?> configuration = new Dictionary<string, string?>()
+        protected static IDictionary<string, string?> configuration = new Dictionary<string, string?>();
+
+        [OneTimeSetUp]
+        public void SetUp()
         {
-            {nameof(Database).ToLower(), Database},
-            {nameof(Endpoint).ToLower(), Endpoint},
-            {nameof(Env).ToLower(), Env},
-            {nameof(Account).ToLower(), Account},
-            {nameof(Engine).ToLower(), Engine},
-            {nameof(ClientId).ToLower(), ClientId},
-            {nameof(ClientSecret).ToLower(), ClientSecret},
-            {nameof(UserName).ToLower(), UserName},
-            {nameof(Password).ToLower(), Password},
-        };
+            Database = EnvWithDefault("FIREBOLT_DATABASE");
+            Endpoint = GetEnvironmentVariable("FIREBOLT_ENDPOINT");
+            Env = EnvWithDefault("FIREBOLT_ENV", "dev");
+            // Endpoint is not specified by CI/CD (YAML) for v2 where account and engine name are mandatory.
+            Account = Endpoint == null ? EnvWithDefault("FIREBOLT_ACCOUNT") : GetEnvironmentVariable("FIREBOLT_ACCOUNT");
+            Engine = Endpoint == null ? EnvWithDefault("FIREBOLT_ENGINE_NAME") : GetEnvironmentVariable("FIREBOLT_ENGINE_NAME");
+            ClientId = GetEnvironmentVariable("FIREBOLT_CLIENT_ID");
+            ClientSecret = GetEnvironmentVariable("FIREBOLT_CLIENT_SECRET");
+            UserName = GetEnvironmentVariable("FIREBOLT_USERNAME");
+            Password = GetEnvironmentVariable("FIREBOLT_PASSWORD");
+            configuration = new Dictionary<string, string?>()
+            {
+                {nameof(Database).ToLower(), Database},
+                {nameof(Endpoint).ToLower(), Endpoint},
+                {nameof(Env).ToLower(), Env},
+                {nameof(Account).ToLower(), Account},
+                {nameof(Engine).ToLower(), Engine},
+                {nameof(ClientId).ToLower(), ClientId},
+                {nameof(ClientSecret).ToLower(), ClientSecret},
+                {nameof(UserName).ToLower(), UserName},
+                {nameof(Password).ToLower(), Password},
+            };
+        }
     }
 }
