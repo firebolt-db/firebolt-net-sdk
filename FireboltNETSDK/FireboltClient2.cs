ef83682c9ae1d0579ecce9f4ef6d2566d546608d
ptiurin <petro.tiurin@firebolt.io>
2024-09-04 17:24:23 +0100

    fetch account id

--- FireboltNETSDK/FireboltClient2.cs
+++ FireboltNETSDK/FireboltClient2.cs
@@ -107,6 +107,7 @@ public class FireboltClient2 : FireboltClient
             systemEngineUrlCache[cacheKey] = systemEngineUrl;
             string[] urlParts = systemEngineUrl.Split('?');
             _connection.EngineUrl = urlParts[0];
+            string? accountId = _connection.AccountId; // initializes InfraVersion and connection.accountId
             if (urlParts.Length > 1)
             {
                 ProcessParameters(new FireboltConnectionStringBuilder(_connection.ConnectionString), ExtractParameters(urlParts[1]), false);
@@ -238,6 +239,6 @@ public class FireboltClient2 : FireboltClient
     public override Task<GetAccountIdByNameResponse> GetAccountIdByNameAsync(string account, CancellationToken cancellationToken)
     {
         // For 2.0 this is a no-op as account id is fetched during use engine command
-        return Task.FromResult(new GetAccountIdByNameResponse() { id = null });
+        return Task.FromResult(new GetAccountIdByNameResponse() { id = null, infraVersion = 2 });
     }
 }
